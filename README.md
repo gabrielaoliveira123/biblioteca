e542cd43a31f19d771e70e444d0d3c1a2599c446
gabrielaoliveira123 <gabriela.07.11.19@gmail.com>
2022-02-10 20:38:41 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # biblioteca
+bem vindos a minha biblioteca!
---
+++ biblioteca.js
@@ -0,0 +1,47 @@
+[
+    {"titulo":"em busca do tempo perdido",
+     "autor":"marcel proust",
+     "ano":"1913-1927"},
+    
+    {"titulo":"ulysses",
+     "autor":"james joyce",
+     "ano":"1922"},
+    
+    {"titulo":"dom quixote",
+     "autor":"miguel de cervantes",
+     "ano":"1605"},
+    
+    {"titulo":"o grande gatsby",
+     "autor":"f. scott fitzgerald",
+     "ano":"1925"},
+    
+    {"titulo":"cem anos de solidao",
+     "autor":"gabriel garcia marquez",
+     "ano":"1967"},
+    
+     {"titulo":"moby dick",
+     "autor":"herman melville",
+     "ano":"1851"},
+    
+     {"titulo":"guerra e paz",
+     "autor":"lev tolstoi",
+     "ano":"1865"},
+    
+     {"titulo":"lolita",
+     "autor":"Vladimir Nabokov",
+     "ano":"1955"},
+    
+     {"titulo":"hamlet",
+     "autor":"william shakespeare",
+     "ano":"1609"},
+    
+     {"titulo":"1984",
+     "autor":"george orwell",
+     "ano":"1949"}
+   
+    ]
+
+    
+    
+     
+    
